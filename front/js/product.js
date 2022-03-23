f7a82b801abd2fc1b09fe1d5a1a90f8dc3296b61
joe9790 <98466882+joe9790@users.noreply.github.com>
2022-03-23 10:35:21 +0100

    Construction du DOM

--- front/js/product.js
+++ front/js/product.js
@@ -4,4 +4,16 @@ const queryString = window.location.search;
 // extraire juste l'id du produits sélectionnés
 let searchParams = new URLSearchParams(queryString);
 const id = searchParams.get("id");
-console.log(id);
+console.log(queryString);
+
+//Construction du DOM
+let url = `http://localhost:3000/api/products/${id}`;
+const dom = {
+  title: document.getElementById("title"),
+  price: document.getElementById("price"),
+  description: document.getElementById("description"),
+  image: document.querySelector(".item__img"),
+  colors: document.getElementById("colors"),
+  quantity: document.getElementById("quantity"),
+  addToCart: document.getElementById("addToCart"),
+};
